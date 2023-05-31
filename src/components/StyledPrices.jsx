100d7480b2789a93a046e74596b5d597ffdb20f7
sueys <sueys@live.fr>
2023-05-31 14:06:45 +0200

    styled emotion my app

---
+++ src/components/StyledPrices.jsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+export const StablePrice = styled.span`
+    font-size:2rem;
+    color: blue;
+`;
+
+export const IncreasedPrice = styled.span`
+    font-size:2rem;
+    color: green;
+`;
+
+export const DecreasedPrice = styled.span`
+    color: red;
+    font-size:2rem;
+`;
